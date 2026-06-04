10e9827f2971b745d10f6c4ea1cb2fec9056c7c5
A1ive <10670106+a1ive@users.noreply.github.com>
2026-06-04 14:46:57 +0900

    Add License

--- fvelib/fvelib.c
+++ fvelib/fvelib.c
@@ -1,4 +1,22 @@
-﻿#include "fvelib.h"
+﻿/*
+ *  FVEUnlocker  --  FVE API Bitlocker Unlock Tool
+ *  Copyright (C) 2026  A1ive.
+ *
+ *  FVEUnlocker is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FVEUnlocker is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FVEUnlocker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "fvelib.h"
 
 #include <wctype.h>
 
